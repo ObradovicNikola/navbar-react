0721faf80cdfa2e863cd5186ac9214495db3c7d9
ObradovicNikola <obradovicnikola009@gmail.com>
2019-09-07 16:57:30 +0200

    Fixed menu margin

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,26 +12,26 @@ export default class Navbar extends Component {
     }
 
     toggleMenu = () => {
-        this.setState({mobileMenuShow: !this.state.mobileMenuShow})
+        this.setState({ mobileMenuShow: !this.state.mobileMenuShow })
     }
 
     closeMenu = () => {
-        this.setState({mobileMenuShow: false})
+        this.setState({ mobileMenuShow: false })
     }
-    
+
     render() {
         let menu;
-        if(this.state.mobileMenuShow)
-        menu = "menu menu-mobile animated fadeInDown"
+        if (this.state.mobileMenuShow)
+            menu = "menu menu-mobile animated fadeInDown"
         else menu = "menu"
         return (
             <nav>
-                <div style={{display: 'flex'}}>
+                <div style={{ display: 'flex' }}>
                     <div className="logo">
                         <Link to="/" className="nav-link" onClick={this.closeMenu}>
-                            <img src={`${process.env.PUBLIC_URL}/images/phoenixLogo.png`} width="50" height="50" alt="logo"/>
+                            <img src={`${process.env.PUBLIC_URL}/images/phoenixLogo.png`} width="50" height="50" alt="logo" />
                             <p>Restaurant Phoenix</p>
-                        </Link> 
+                        </Link>
                     </div>
                     <div className="burger" onClick={this.toggleMenu}>
                         <i className="fas fa-bars"></i>
